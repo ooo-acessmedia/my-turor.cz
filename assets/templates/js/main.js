32f2e500addf83dd3318cdca6dfbcb8c92d9d3eb
romikon164 <romikon164@gmail.com>
2016-08-11 16:23:58 +0400

    Style and scripts for user panel

--- assets/templates/js/main.js
+++ assets/templates/js/main.js
@@ -91,6 +91,7 @@
         $('.clockpicker').clockpicker({
             autoclose: true
         });
+        /* ajax загрузка изображений */
         $('.ajax-image-uploader').each(function (i, elem) {
             $(elem).find('.ajax-image-file').on('change', function () {
                 $(elem).find('.ajax-image-name').val($(this).val().split("/").pop().split("\\").pop());
@@ -109,6 +110,7 @@
                     data: data,
                     cache: false,
                     dataType: 'json',
+                    async: false,
                     processData: false, // Не обрабатываем файлы (Don't process the files)
                     contentType: false, // Так jQuery скажет серверу что это строковой запрос
                     success: function( respond, textStatus, jqXHR ){
@@ -129,6 +131,126 @@
                     }
                 });
             });
+        });
+        /* ajax формы фильтрации */
+        $(document).on('submit', '.ajaxFilter', function () {
+            var url = $(this).attr('action');
+            var filterContain = $(this).attr('data-filter-contain');
+            var resultContain = $(this).attr('data-result-contain');
+            var gets = {};
+            var newUrl = location.href.split('?');
+            if(newUrl.length > 1) {
+                newUrl = newUrl[0];
+                newUrl[1].split('&').forEach(function (g) {
+                    if(g) {
+                        g = g.split('=');
+                        gets[g[0]] = g.length > 1 ? g[1] : "";
+                    }
+                });
+            }
+            $(this).find('select,teaxtarea,input,checkbox,radiobox').each(function (i, inp) {
+                var val = $(inp).val();
+                var name = $(inp).attr('name');
+                delete gets[name];
+                if(val) {
+                    gets[name] = val;
+                }
+            });
+            var getstr = "";
+            for(var k in gets) {
+                getstr += "&" + k + "=" + gets[k];
+            }
+            if(getstr !== "") {
+                newUrl += "?" + getstr.substr(1);
+            }
+            $.ajax({
+                url: url,
+                data: gets,
+                method: 'GET',
+                success: function (html) {
+                    $(filterContain).html($(html).find(filterContain).html());
+                    $(resultContain).html($(html).find(resultContain).html());
+                    history.pushState(null, document.title, newUrl)
+                }
+            });
+            return false;
+        });
+
+        $("[data-toggle='tooltip']").tooltip();
+        
+        $(document).on('click', '.action-remove', function () {
+            var removeItem = $(this).attr('data-remove');
+            $(removeItem).remove();
+            return false;
+        });
+        
+        $(document).on('click', '.action-remove-all', function () {
+            var removeItem = $(this).attr('data-remove');
+            var conditionItem = $(this).attr('data-condition');
+            $(removeItem).each(function (i, elem) {
+                if($(elem).find(conditionItem).length) {
+                    $(elem).remove();
+                }
+            });
+        });
+        
+        $(document).on('click', '.action-select-all', function () {
+            var selectItems = $(this).attr('data-select-all');
+            if($(this).is(':checked')) {
+                $(selectItems).attr('checked', true);
+            } else {
+                $(selectItems).removeAttr('checked');
+            }
+        });
+        
+        $(document).on('click', '.action-add', function () {
+            var id = 0;
+            $('.service-item').each(function (i, elem) {
+                var _id = $(elem).attr('id').split('-').pop();
+                if($.isNumeric(_id) && _id > id) id = _id;
+            });
+            id++;
+            var $row = $(myTutor.chunks.teacherUserPanel.service)
+                .attr('id', 'service-' + id)
+                .appendTo($('#service-list'));
+            $row.find('input[name="services_id[]"]').attr('name', 'services_id[new_' + id +']').val('new_' + id);
+            $row.find('input[name="services_name[]"]').attr('name', 'services_name[new_' + id +']').val('Новая услуга');
+            $row.find('input[name="services_price[]"]').attr('name', 'services_price[new_' + id +']').val('0');
+            $row.find('input[name="services_active[]"]').attr('name', 'services_active[new_' + id +']').val('1');
+            $row.find('.action-remove').attr('data-remove', '#service-' + id);
+            return false;
+        });
+
+        if(location.hash) {
+            $('a[href="' + location.hash + '"]').tab('show');
+            if(location.hash == '#learner-active' || location.hash == '#learner-wait') {
+                $('a[href="#my-learner"]').tab('show');
+            }
+        }
+            
+        $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function () {
+            history.pushState(null, "", (location.href.split('#'))[0] + $(this).attr('href'));
+        });
+        
+        $(document).on('submit', '.ajaxFormNonResponce', function () {
+            $.ajax({
+                url: $(this).attr('action'),
+                method: $(this).attr('method'),
+                data: $(this).serialize(),
+                dataType: 'json',
+                success: function (ids) {
+                    ids.forEach(function (id, i) {
+                        var $item = $('.service-item').eq(i);
+                        $item.attr('id', 'service-' + id);
+                        $item.find('td').eq(1).html(id);
+                        $item.find('input[name^="services_id"]').attr('name', 'services_id[' + id + ']').val(id);
+                        $item.find('input[name^="services_name"]').attr('name', 'services_name[' + id + ']');
+                        $item.find('input[name^="services_price"]').attr('name', 'services_price[' + id + ']');
+                        $item.find('input[name^="services_active"]').attr('name', 'services_active[' + id + ']');
+                    });
+                }
+            });
+            return false;
         });
     });
 
